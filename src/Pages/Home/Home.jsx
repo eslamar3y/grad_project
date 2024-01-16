51b19368b98a0e28aa98fe025b4d726fa0d52f8b
Ahmed-Reda-elshahawy <ahmed92.5elshahawy@gmail.com>
2024-01-16 10:10:23 +0200

    some structure

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <div className="bg-slate-400 min-h-screen">Home</div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
+import Login from './Pages/Login/Login.jsx';
+import Register from './Pages/Register/Register.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
