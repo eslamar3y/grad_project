d6da81ed8516e1702be144c41ee8a3287813b981
eslam <emaree86@gmail.com>
2024-02-03 22:22:47 +0200

    Add upload image page

--- src/Pages/DiseaseDetection/DiseaseDetection.jsx
+++ src/Pages/DiseaseDetection/DiseaseDetection.jsx
@@ -1,9 +1,54 @@
 import SimpleNav from "../../components/SimpleNav";
+import vector from "../../assets/vector.png";
 
 const DiseaseDetection = () => {
+  // detect if file uploaded
+  const fileUpload = (e) => {
+    console.log(e.target.files[0]);
+    // if uploaded file is not an image dont proceed and dont upload it
+    if (e.target.files[0].type.split("/")[0] !== "image") {
+      alert("Please upload an image");
+      return;
+    }
+
+    // show the name of the uploaded image
+    document.querySelector(".imgName").innerText = e.target.files[0].name;
+  };
+
   return (
-    <div className="">
+    <div className="bg-[#D9D9D9] w-full h-lvh">
       <SimpleNav />
+      <div className="detectBody ml-[10%] mt-[3%] rounded-xl border border-dotted border-black bg-white w-[80%] h-[60%] relative z-[0]">
+        <form action="#">
+          <img
+            src={vector}
+            alt=""
+            className="xs:h-[90px] xs:mt-24 md:h-[130px] mx-auto mt-[8%] hover:opacity-60"
+          />
+          <p className="text-center font-popins text-gray-500 mt-2">
+            Drag and drop image here
+          </p>
+          <p className="text-center font-popins text-gray-500 mt-6">
+            OR
+            <br />
+            <input
+              type="file"
+              className="opacity-0 absolute top-0 left-0 w-[100%] h-[100%] cursor-pointer"
+              onChange={fileUpload}
+            />
+            <button className="SignInButon  font-popins mx-auto px-12 py-3 text-white rounded-2xl mt-4">
+              Import Image
+            </button>
+          </p>
+          <p className="text-center text-sm font-popins text-gray-500 mt-3 mb-5 imgName"></p>
+          <button
+            type="submit"
+            className="bg-[#585ec7] hover:bg-[#4d53c7]  font-popins mx-auto px-12 py-3 text-white rounded absolute -bottom-24 xs:left-[16%] md:left-[42%] "
+          >
+            Show results
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,47 +2,81 @@ import { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
-
 const NavLinks = () => {
-    return (
-        <>
-            <NavLink to='/' className={({ isActive }) => (isActive ? ' font-bold text-lg' : '')}>Home</NavLink>
-            <NavLink to='detection' className={({ isActive }) => (isActive ? ' font-bold text-xl' : '')}>Disease Detection</NavLink>
-            <NavLink to='equipments' className={({ isActive }) => (isActive ? ' font-bold text-xl' : '')}>Farm Equipments</NavLink>
-            <NavLink to='fishInfo' className={({ isActive }) => (isActive ? ' font-bold text-xl' : '')}>Fish Information</NavLink>
-            <NavLink to='doctors' className={({ isActive }) => (isActive ? ' font-bold text-xl' : '')}>Doctors</NavLink>
-            <NavLink to='about' className={({ isActive }) => (isActive ? ' font-bold text-xl' : '')}>About</NavLink>
-            <NavLink to='chat' className={({ isActive }) => (isActive ? ' font-bold text-xl' : '')}>Q&A, Chat</NavLink>
-        </>
-    )
-}
-
+  return (
+    <>
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? " font-bold text-lg" : "")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="detection"
+        className={({ isActive }) => (isActive ? " font-bold text-xl" : "")}
+      >
+        Disease Detection
+      </NavLink>
+      <NavLink
+        to="equipments"
+        className={({ isActive }) => (isActive ? " font-bold text-xl" : "")}
+      >
+        Farm Equipments
+      </NavLink>
+      <NavLink
+        to="fishInfo"
+        className={({ isActive }) => (isActive ? " font-bold text-xl" : "")}
+      >
+        Fish Information
+      </NavLink>
+      <NavLink
+        to="doctors"
+        className={({ isActive }) => (isActive ? " font-bold text-xl" : "")}
+      >
+        Doctors
+      </NavLink>
+      <NavLink
+        to="about"
+        className={({ isActive }) => (isActive ? " font-bold text-xl" : "")}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="chat"
+        className={({ isActive }) => (isActive ? " font-bold text-xl" : "")}
+      >
+        Q&A, Chat
+      </NavLink>
+    </>
+  );
+};
 
 export default function Nav() {
-    const [showMenu, setShowMenu] = useState(false);
-    const handleShowMenu = () => {
-        setShowMenu((prev) => !prev);
-    }
+  const [showMenu, setShowMenu] = useState(false);
+  const handleShowMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
 
-
-    return (
-        <>
-            <nav>
-                <div className="hidden 2xl:flex 2xl:gap-6 2xl:items-center">
-                    <NavLinks />
-                </div>
-                <button className=' flex align-top 2xl:hidden'><FaBars className='text-2xl' onClick={handleShowMenu} /></button>
-            </nav>
-            {showMenu &&
-                <menu className="flex flex-col w-full h-[610px] gap-5 mt-5 bg-white p-4 rounded-lg absolute top-5 right-0">
-                    <NavLinks />
-                    <NavLink to='login'>
-                        <button className='w-[90%] h-[48px] absolute left-[50%] translate-x-[-50%] bottom-6 mt-5 shadow-custom border-black bg-secondColor text-white rounded-2xl'>
-                            Signin
-                        </button>
-                    </NavLink>
-                </menu>
-            }
-        </>
-    )
+  return (
+    <>
+      <nav>
+        <div className="hidden 2xl:flex 2xl:gap-6 2xl:items-center">
+          <NavLinks />
+        </div>
+        <button className=" flex align-top 2xl:hidden">
+          <FaBars className="text-2xl" onClick={handleShowMenu} />
+        </button>
+      </nav>
+      {showMenu && (
+        <menu className="flex flex-col w-full h-[610px] gap-5 mt-5 bg-white p-4 rounded-lg absolute top-5 right-0 z-[9] ">
+          <NavLinks />
+          <NavLink to="login">
+            <button className="w-[90%] h-[48px] absolute left-[50%] translate-x-[-50%] bottom-6 mt-5 shadow-custom border-black bg-secondColor text-white rounded-2xl">
+              Signin
+            </button>
+          </NavLink>
+        </menu>
+      )}
+    </>
+  );
 }
