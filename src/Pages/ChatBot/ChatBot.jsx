081f5417bf6344c515a1efba3c6a2ba5a8e98e9f
Ahmed-Reda-elshahawy <ahmed92.5elshahawy@gmail.com>
2024-02-13 13:21:20 +0200

    update design of chatbot

--- src/Pages/ChatBot/ChatBot.jsx
+++ src/Pages/ChatBot/ChatBot.jsx
@@ -1,19 +1,21 @@
-import { BsSearch } from "react-icons/bs";
-import { FaPhoneAlt } from "react-icons/fa";
-import { BsThreeDotsVertical } from "react-icons/bs";
-import { IoMdAddCircleOutline } from "react-icons/io";
-import { FaBars } from "react-icons/fa";
+// import { BsSearch } from "react-icons/bs";
+// import { FaPhoneAlt } from "react-icons/fa";
+// import { BsThreeDotsVertical } from "react-icons/bs";
+// import { IoMdAddCircleOutline } from "react-icons/io";
+// import { FaBars } from "react-icons/fa";
+import { MdHome } from "react-icons/md";
 import { IoSend } from "react-icons/io5";
 import chat from "../../assets/chat-bot.png";
 import styles from "./ChatBot.module.css";
+import { Link } from "react-router-dom";
 
 
 export default function ChatBot() {
     return (
         <main className="min-h-svh bg-[#585dc7da] bg-chatBot flex">
-            <aside className="w-20 lg:w-[300px] min-h-svh bg-mainColor relative border-r-2 px-5 py-4">
+            {/* <aside className="w-20 lg:w-[300px] min-h-svh bg-mainColor relative border-r-2 px-5 py-4">
                 <FaBars className="text-2xl cursor-pointer" />
-            </aside>
+            </aside> */}
             <section className="w-full">
                 <header className="flex justify-between bg-mainColor px-6 py-2">
                     <div className="flex gap-4 items-center">
@@ -24,17 +26,20 @@ export default function ChatBot() {
                         </div>
                     </div>
                     <div className="flex gap-6 items-center">
-                        <BsSearch className="text-gray-600" />
+                        <Link to="/">
+                            <MdHome className="text-gray-600 text-2xl cursor-pointer" />
+                        </Link>
+                        {/* <BsSearch className="text-gray-600" />
                         <FaPhoneAlt className="text-gray-600" />
-                        <BsThreeDotsVertical className="text-gray-800" />
+                        <BsThreeDotsVertical className="text-gray-800" /> */}
                     </div>
                 </header>
                 <article className={styles['chat-section']}></article>
-                <footer className="bg-mainColor px-4 py-3 flex items-center gap-4">
-                    <IoMdAddCircleOutline className="text-gray-400 text-4xl" />
+                <section className="bg-mainColor px-4 py-3 flex items-center gap-4">
+                    {/* <IoMdAddCircleOutline className="text-gray-400 text-4xl" /> */}
                     <input type="text" name="chat-text" id="chat-text" className="w-full p-3 rounded-xl shadow-inner" placeholder="Type Your Message" />
                     <IoSend className=" text-4xl text-white p-2 bg-gray-400 rounded-full" />
-                </footer>
+                </section>
             </section>
         </main>
     )
