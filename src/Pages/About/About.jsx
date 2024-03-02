586800b374e30cdaa0fc5783c9cba3dc7e485c2a
eslam <emaree86@gmail.com>
2024-03-02 19:18:03 +0200

    make about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,139 +3,99 @@ import teammeamber1 from "../../assets/teammeamber1.png";
 
 const About = () => {
   return (
-    // <div className="bg-[#D9D9D9] w-full ">
-    //   <SimpleNav />
-    //   <div className="flex">
-    //     <div className="w-1/2 pt-5 pl-12 ">
-    //       <div className="ourteam w-[608px] h-[608px] rounded-full bg-[#585ec7]">
-    //         <div className="pt-44  text-white font-popins text-center">
-    //           <h1 className="text-5xl font-bold ">Our Team</h1>
-    //           <p className="font-medium text-lg leading-9 text px-3 pt-6 tracking-wide">
-    //             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-    //             auctor nisl sit amet nisi fermentum, at bibendum mauris
-    //             pulvinar.
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="w-1/2 flex flex-wrap">
-    //       <div className="mt-14 text-center">
-    //         <img
-    //           src={teammeamber1}
-    //           alt=""
-    //           className="w-[200px] h-[197px] rounded-full"
-    //         />
-    //         <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
-    //           Calvin Cooper
-    //         </h3>
-    //         <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
-    //       </div>
-    //       <div className="mt-14 text-center ml-16">
-    //         <img
-    //           src={teammeamber1}
-    //           alt=""
-    //           className="w-[200px] h-[197px] rounded-full"
-    //         />
-    //         <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
-    //           Calvin Cooper
-    //         </h3>
-    //         <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
-    //       </div>
-    //       <div className="mt-14 text-center ml-16">
-    //         <img
-    //           src={teammeamber1}
-    //           alt=""
-    //           className="w-[200px] h-[197px] rounded-full"
-    //         />
-    //         <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
-    //           Calvin Cooper
-    //         </h3>
-    //         <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
-    //       </div>
-    //       {/* second row */}
-    //       <div className="mt-9 text-center">
-    //         <img
-    //           src={teammeamber1}
-    //           alt=""
-    //           className="w-[200px] h-[197px] rounded-full"
-    //         />
-    //         <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
-    //           Calvin Cooper
-    //         </h3>
-    //         <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
-    //       </div>
-    //       <div className="mt-9 text-center ml-16">
-    //         <img
-    //           src={teammeamber1}
-    //           alt=""
-    //           className="w-[200px] h-[197px] rounded-full"
-    //         />
-    //         <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
-    //           Calvin Cooper
-    //         </h3>
-    //         <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
-    //       </div>
-    //       <div className="mt-9 text-center ml-16">
-    //         <img
-    //           src={teammeamber1}
-    //           alt=""
-    //           className="w-[200px] h-[197px] rounded-full"
-    //         />
-    //         <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
-    //           Calvin Cooper
-    //         </h3>
-    //         <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
-    //       </div>
-    //       {/* second row */}
-    //       <div className="mt-9 text-center">
-    //         <img
-    //           src={teammeamber1}
-    //           alt=""
-    //           className="w-[200px] h-[197px] rounded-full"
-    //         />
-    //         <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
-    //           Calvin Cooper
-    //         </h3>
-    //         <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
-    //       </div>
-    //       <div className="mt-9 text-center ml-16">
-    //         <img
-    //           src={teammeamber1}
-    //           alt=""
-    //           className="w-[200px] h-[197px] rounded-full"
-    //         />
-    //         <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
-    //           Calvin Cooper
-    //         </h3>
-    //         <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
-    //       </div>
-    //       <div className="mt-9 text-center ml-16">
-    //         <img
-    //           src={teammeamber1}
-    //           alt=""
-    //           className="w-[200px] h-[197px] rounded-full"
-    //         />
-    //         <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
-    //           Calvin Cooper
-    //         </h3>
-    //         <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
-    //       </div>
-    //       {/* second row */}
-    //       <div className="mt-9 text-center">
-    //         <img
-    //           src={teammeamber1}
-    //           alt=""
-    //           className="w-[200px] h-[197px] rounded-full"
-    //         />
-    //         <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
-    //           Calvin Cooper
-    //         </h3>
-    //         <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="">about</div>
+    <div className="bg-[#D9D9D9] w-full min min-h-screen">
+      <SimpleNav />
+      <div className="flex flex-wrap mx-64">
+        <div className="mt-14 text-center">
+          <img
+            src={teammeamber1}
+            alt=""
+            className="w-[200px] h-[197px] rounded-full"
+          />
+          <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
+            Calvin Cooper
+          </h3>
+          <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
+        </div>
+        <div className="mt-14 text-center ml-16">
+          <img
+            src={teammeamber1}
+            alt=""
+            className="w-[200px] h-[197px] rounded-full"
+          />
+          <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
+            Calvin Cooper
+          </h3>
+          <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
+        </div>
+        <div className="mt-14 text-center ml-16">
+          <img
+            src={teammeamber1}
+            alt=""
+            className="w-[200px] h-[197px] rounded-full"
+          />
+          <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
+            Calvin Cooper
+          </h3>
+          <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
+        </div>
+        <div className="mt-14 text-center ml-16">
+          <img
+            src={teammeamber1}
+            alt=""
+            className="w-[200px] h-[197px] rounded-full"
+          />
+          <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
+            Calvin Cooper
+          </h3>
+          <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
+        </div>
+        <div className="mt-14 text-center">
+          <img
+            src={teammeamber1}
+            alt=""
+            className="w-[200px] h-[197px] rounded-full"
+          />
+          <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
+            Calvin Cooper
+          </h3>
+          <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
+        </div>
+        <div className="mt-14 text-center ml-16">
+          <img
+            src={teammeamber1}
+            alt=""
+            className="w-[200px] h-[197px] rounded-full"
+          />
+          <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
+            Calvin Cooper
+          </h3>
+          <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
+        </div>
+        <div className="mt-14 text-center ml-16">
+          <img
+            src={teammeamber1}
+            alt=""
+            className="w-[200px] h-[197px] rounded-full"
+          />
+          <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
+            Calvin Cooper
+          </h3>
+          <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
+        </div>
+        <div className="mt-14 text-center ml-16">
+          <img
+            src={teammeamber1}
+            alt=""
+            className="w-[200px] h-[197px] rounded-full"
+          />
+          <h3 className="text-[#585EC7] font-semibold font-popins text-lg mt-7">
+            Calvin Cooper
+          </h3>
+          <h5 className="text-white font-popins text-sm ">Randall Warren</h5>
+        </div>
+      </div>
+    </div>
   );
 };
 
